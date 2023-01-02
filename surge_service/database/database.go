1aa25e3a344efb16c6bbc1a36972b0f55c13add1
Paria Zeynali <pariazeynali1999@gmail.com>
2023-01-02 17:22:21 +0330

    Code refactoring

---
+++ surge_service/database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"os"
+)
+
+var Conn *pgxpool.Pool
+
+func ConnectToDb() {
+	dbUser := os.Getenv("OSM_USER")
+	dbPass := os.Getenv("OSM_PASS")
+	dbName := os.Getenv("OSM_DB")
+	dbHost := os.Getenv("OSM_HOST")
+	dbPort := os.Getenv("OSM_PORT")
+
+	connStr := fmt.Sprintf("user=%v password=%v host=%v port=%v dbname=%v pool_max_conns=50", dbUser, dbPass, dbHost, dbPort, dbName)
+	var err error
+	Conn, err = pgxpool.New(context.Background(), connStr)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
+}
+
+func CreateDBRelations() {
+	sql, err := os.ReadFile("./views.sql")
+	if err != nil {
+		log.Fatalf("ERROR reading SQl files: %v", err)
+	}
+	sqlQ := string(sql)
+	c, _ := Conn.Acquire(context.Background())
+	defer c.Release()
+	_, err = c.Exec(context.Background(), sqlQ)
+	if err != nil {
+		log.Fatalf("ERROR could not create views: %v", err)
+	}
+}
